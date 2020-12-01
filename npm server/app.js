29da26df053dc9303bc67ddfc50a16a8c3af4a42
balamurugan213 <balainfo21@gmail.com>
2020-12-01 11:40:30 +0530

    adding mongo DB

--- npm server/app.js
+++ npm server/app.js
@@ -1,16 +1,51 @@
 const express=require('express');
-const { cond } = require('lodash');
+const { cond, result } = require('lodash');
 const morgan=require('morgan');
+const mongoose=require('mongoose');
+const Blog=require('./models/blog')
 
 // express app
 const app = express();
+
+// connecting mongo db
+const DB_url='mongodb+srv://bala:hamlet213@blogpost.v7fd1.mongodb.net/Hamlet?retryWrites=true&w=majority';
+
+mongoose.connect(DB_url, { useNewUrlParser: true, useUnifiedTopology: true }).then((result) => app.listen(3000))
+.catch((err) => console.log('err',err));
+
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // listening for request
-app.listen(3000);
+// app.listen(3000);
 
+/* sandbox routes
+app.get('/add-blog',(req, res)=>{
+    const blog= new Blog({
+        title:'new blog',
+        snippet:'abt hamlet',
+        body:'hamlet is just a name'
+    });
+
+    blog.save().then((result)=>{
+        res.send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+})
+
+app.get('/all-blog',(req, res)=>{
+    
+    Blog.find().then((result)=>{
+        res.send(result)
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    //Blog.findById()
+ })*/
 
 // middleware and static
 app.use(express.static('assets'))
@@ -32,13 +67,21 @@ app.use(morgan('dev'));
 app.get('/',(req,res) =>{
     // res.send('<p>Home page</p>');
     // res.sendFile('./views/index.html',{root: __dirname});
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
+    // const blogs = [
+    //     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //   ];
+    // res.render('index',{ title:'Home',blogs:blogs});
+    
+    
+    Blog.find().sort({createdAt:-1})
+    .then((result)=>{
+        res.render('index',{title:'All Blogs',blogs:result})
+    })
+    .catch((err)=>console.log(err));
 
-    res.render('index',{ title:'Home',blogs:blogs});
+   
 });
 
 app.get('/about',(req,res) =>{
